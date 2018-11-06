a7c1104521ae154dd4e5a127e0966a0b37bf2fca
brandao123 <gabrielcandidobrandao@gmail.com>
2018-11-06 20:34:37 -0200

    tres

---
+++ wget-log.sh
@@ -0,0 +1,6 @@
+--2018-10-30 21:02:11--  https://www.google.com/search?q=carro
+Resolvendo www.google.com (www.google.com)... 216.58.202.228, 2800:3f0:4004:800::2004
+Conectando-se a www.google.com (www.google.com)|216.58.202.228|:443... conectado.
+A requisição HTTP foi enviada, aguardando resposta... 403 Forbidden
+2018-10-30 21:02:13 ERRO 403: Forbidden.
+
